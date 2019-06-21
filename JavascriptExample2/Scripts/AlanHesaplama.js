ff296d0c150a0b0ae7aa2e1ad9c15b587ee4bd3d
UĞUR BAĞCI <ugur_bagci_@hotmail.com>
2019-06-21 16:33:25 +0300

    Add project files.

---
+++ JavascriptExample2/Scripts/AlanHesaplama.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $.AlanHesapla = {
+        Alan: function (x, y) {
+            return x * y;
+        },
+        Cevre: function (x, y) {
+            return 2 * (x + y);
+        }
+    };
+
+
+});
---
+++ JavascriptExample2/Scripts/HesapMakinesi.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $.HesapMakinesi = {
+        Toplama: function (a, b) {
+            return a + b;
+        },
+        Cikarma: function (a, b) {
+            return a - b;
+        },
+        Carpma: function (a, b) {
+            return a * b;
+        },
+        Bolme: function (a, b) {
+            return a / b;
+        }
+    };
+});
